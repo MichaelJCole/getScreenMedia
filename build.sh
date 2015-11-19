73deb53cfc7c51e6b3dae64293343e0f3f9b814f
Michael Cole <michaelcole@michaelcole.com>
2015-11-19 16:06:09 -0600

    Build new version

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo
 echo
 echo "*************************************************************************"
 echo "********* DO NOT FORGET TO UPDATE firefoxExtensionHash IN settings.json "
-echo "********* OR SCREEN SHARING INSTALLATION WILL FAIL"
+echo "********* AFTER EXTENSION IS REVIEWED:"
+echo "https://addons.mozilla.org/firefox/downloads/latest/669945/addon-669945-latest.xpi"
+echo "sha1sum addon-669945-latest.xpi"
 echo "*************************************************************************"
-
-sha1sum *.xpi 
